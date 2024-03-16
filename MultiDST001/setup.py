c0daff5228939da5c303286b0134d6997262f378
Susara Ouchithya <susara.ouchithya@gmail.com>
2024-03-16 10:46:46 +0530

    venv it boy

--- MultiDST001/setup.py
+++ MultiDST001/setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='MultiDST - package',
-    version='0.1',
+    name='multidst_001',
+    version='0.0.1',
     packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "numpy"
+    ],
+    author="Susara Ouchithya",
+    description="Multiple Testing made easy!",
+    long_description=open("README.md").read(),
+    long_description_content_type = "text/markdown"
 )
